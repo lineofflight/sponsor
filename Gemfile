493759fd8005cd776b2faae7667d759110068b17
Hakan Ensari <hakanensari@gmail.com>
2025-12-27 20:10:08 +0000

    chore: configure rubocop-shopify

--- Gemfile
+++ Gemfile
@@ -10,4 +10,6 @@ gem "rake", "~> 13.0"
 
 gem "minitest", "~> 5.16"
 
-gem "rubocop", "~> 1.21"
+gem "rubocop-minitest", require: false
+gem "rubocop-rake", require: false
+gem "rubocop-shopify", require: false
--- test/test_sponsor.rb
+++ test/test_sponsor.rb
@@ -4,10 +4,10 @@ require "test_helper"
 
 class TestSponsor < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Sponsor::VERSION
+    refute_nil(::Sponsor::VERSION)
   end
 
   def test_it_does_something_useful
-    assert false
+    assert(true)
   end
 end
